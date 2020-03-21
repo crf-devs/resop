c4568b2c5ccf69c775e60b75c8c322665f3ff5a8
Hugo Hamon <hugohamon@neuf.fr>
2020-03-21 23:13:02 +0100

    Add `CommissionableAsset` and `CommissionableAssetAvailability` entity mappings

---
+++ src/Repository/CommissionableAssetAvailabilityRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\CommissionableAsset;
+use App\Entity\CommissionableAssetAvailability;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+/**
+ * @method CommissionableAsset|null find($id, $lockMode = null, $lockVersion = null)
+ * @method CommissionableAsset|null findOneBy(array $criteria, array $orderBy = null)
+ * @method CommissionableAsset[]    findAll()
+ * @method CommissionableAsset[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CommissionableAssetAvailabilityRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, CommissionableAssetAvailability::class);
+    }
+}
