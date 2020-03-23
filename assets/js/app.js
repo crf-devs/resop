b31e05fde0b4b4b9a0902b663e5c8d8687fc89c1
Michel Roca <mroca.dh@gmail.com>
2020-03-24 00:26:50 +0100

    Add bootrasp select (#92)

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,10 @@ import '../css/planning.scss';
 
 const $ = require('jquery');
 require('bootstrap');
+require('bootstrap-select');
+
+$.fn.selectpicker.Constructor.DEFAULTS.noneSelectedText = '-';
+$.fn.selectpicker.Constructor.DEFAULTS.noneResultsText = 'Aucun résultat pour {0}';
+$.fn.selectpicker.Constructor.DEFAULTS.selectAllText = 'Tout sélectionner';
+$.fn.selectpicker.Constructor.DEFAULTS.deselectAllText = 'Tout déselectionner';
+$.fn.selectpicker.Constructor.DEFAULTS.doneButtonText = 'Fermer';
