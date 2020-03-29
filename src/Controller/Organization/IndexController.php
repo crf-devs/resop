2b311fc9cb0515e2bad53fe1f3b02ce05485b82a
Fabien MEYNARD <fabien.meynard@gmail.com>
2020-03-29 12:39:31 +0100

    organizations management for parent structures

--- src/Controller/Organization/IndexController.php
+++ src/Controller/Organization/IndexController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("", name="app_organization_index")
+ * @Route("/dashboard", name="app_organization_index", methods={"GET"})
  */
 final class IndexController extends AbstractController
 {
