0177ab3ae1ad5a1af0743dd9c8b75ab5a3dd780f
Fabien MEYNARD <fabien.meynard@gmail.com>
2020-03-29 21:38:48 +0100

    login enhancements (#178)
    * login enhancements

--- src/Controller/User/Security/LoginController.php
+++ src/Controller/User/Security/LoginController.php
@@ -8,6 +8,7 @@ use App\Entity\User;
 use App\Form\Type\UserLoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -23,16 +24,16 @@ final class LoginController extends AbstractController
         $this->authenticationUtils = $authenticationUtils;
     }
 
-    public function __invoke(): Response
+    public function __invoke(Session $session): Response
     {
         if (is_object($this->getUser())) {
             return $this->redirectToRoute($this->getUser() instanceof User ? 'user_home' : 'app_organization_index');
         }
 
-        $loginForm = $this->createForm(
-            UserLoginType::class,
-            ['identifier' => $this->authenticationUtils->getLastUsername()]
-        );
+        $loginForm = $this->createForm(UserLoginType::class, [
+            'identifier' => $this->authenticationUtils->getLastUsername(),
+            'birthday' => $session->get('_security.last_birthday', '1990-01-01'),
+        ]);
 
         return $this->render('user/login.html.twig', [
             'loginForm' => $loginForm->createView(),
