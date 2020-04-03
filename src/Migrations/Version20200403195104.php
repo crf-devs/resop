543da1b8271c55d2be7284343cfc989f96ce4b9a
Michel Roca <mroca.dh@gmail.com>
2020-04-03 21:59:01 +0200

    Persist session in db

--- src/Migrations/Version20200403195104.php
+++ src/Migrations/Version20200403195104.php
@@ -7,19 +7,24 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20200330155339 extends AbstractMigration
+final class Version20200403195104 extends AbstractMigration
 {
+    public function getDescription(): string
+    {
+        return 'Add session table';
+    }
+
     public function up(Schema $schema): void
     {
         $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('ALTER TABLE commissionable_asset ALTER has_mobile_radio SET NOT NULL');
+        $this->addSql('CREATE TABLE sessions ( sess_id VARCHAR(128) NOT NULL PRIMARY KEY, sess_data BYTEA NOT NULL, sess_time INTEGER NOT NULL, sess_lifetime INTEGER NOT NULL );');
     }
 
     public function down(Schema $schema): void
     {
         $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('ALTER TABLE commissionable_asset ALTER has_mobile_radio DROP NOT NULL');
+        $this->addSql('DROP TABLE sessions');
     }
 }
