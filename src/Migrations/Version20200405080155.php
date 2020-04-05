875d34ec260392f82190c7dfa631dfd20d12bed6
Vincent Chalamon <vincentchalamon@gmail.com>
2020-04-05 10:02:25 +0200

    Add Doctrine migration

---
+++ src/Migrations/Version20200405080155.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200405080155 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('DROP INDEX organisation_name_unique');
+        $this->addSql('CREATE INDEX organization_name_idx ON organization (name)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('DROP INDEX organization_name_idx');
+        $this->addSql('CREATE UNIQUE INDEX organisation_name_unique ON organization (name)');
+    }
+}
