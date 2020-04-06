f7782a3f7a6e124ead146456a8645bcfb2584cb0
Vincent Chalamon <vincentchalamon@gmail.com>
2020-04-06 17:58:45 +0200

    Add User planning behat tests

---
+++ features/user/availabilities.feature
@@ -0,0 +1,26 @@
+Feature:
+    In order to fill in my availabilities
+    As a user
+    I must be able to create my account
+
+    Scenario: As authenticated user, I can update my availabilities
+        Given I am authenticated as a user
+        And I am on "/"
+        When I follow "Mes disponibilités pour la semaine prochaine"
+        And I check "tuesday" column
+        And I press "Enregistrer mes disponibilités"
+        Then I should be on "/"
+        And I should see "Vos disponibilités ont été mises à jour avec succès."
+        Then I follow "Mes disponibilités pour la semaine prochaine"
+        And column "tuesday" should be checked
+
+    Scenario: As authenticated user, I can remove my availabilities
+        Given I am authenticated as a user
+        And I am on "/"
+        When I follow "Mes disponibilités pour la semaine prochaine"
+        And I uncheck "tuesday" column
+        And I press "Enregistrer mes disponibilités"
+        Then I should be on "/"
+        And I should see "Vos disponibilités ont été mises à jour avec succès."
+        Then I follow "Mes disponibilités pour la semaine prochaine"
+        And column "tuesday" should be unchecked
---
+++ tests/Behat/UserPlanningContext.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Behat;
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\Environment\InitializedContextEnvironment;
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
+use Behat\Mink\Exception\ElementNotFoundException;
+use Behat\Mink\Exception\ExpectationException;
+use Behat\MinkExtension\Context\MinkContext;
+
+final class UserPlanningContext implements Context
+{
+    private MinkContext $minkContext;
+
+    /**
+     * @BeforeScenario
+     */
+    public function gatherContext(BeforeScenarioScope $scope): void
+    {
+        /** @var InitializedContextEnvironment $environment */
+        $environment = $scope->getEnvironment();
+        /** @var MinkContext $minkContext */
+        $minkContext = $environment->getContext(MinkContext::class);
+        $this->minkContext = $minkContext;
+    }
+
+    /**
+     * @When /^I (?P<action>(?:check|uncheck)) "(?P<day>(?:monday|tuesday|wednesday|thursday|friday|saturday|sunday))" column$/
+     */
+    public function checkColumn(string $action, string $day): void
+    {
+        $page = $this->minkContext->getSession()->getPage();
+        /** @var int $time */
+        $time = strtotime($day);
+        $elements = $page->findAll('css', sprintf('table.availability-form-table tbody td[data-day="%d"] input[type="checkbox"]', (int) date('w', $time) + 1));
+        if (0 === \count($elements)) {
+            throw new ElementNotFoundException($this->minkContext->getSession()->getDriver(), 'form field', 'id|name|label|value', $day);
+        }
+
+        foreach ($elements as $element) {
+            if ('check' === $action) {
+                $element->check();
+            } else {
+                $element->uncheck();
+            }
+        }
+    }
+
+    /**
+     * @When /^(?:the )?column "(?P<day>(?:monday|tuesday|wednesday|thursday|friday|saturday|sunday))" should be (?P<action>(?:checked|unchecked))$/
+     */
+    public function verifyColumn(string $day, string $state): void
+    {
+        $page = $this->minkContext->getSession()->getPage();
+        /** @var int $time */
+        $time = strtotime($day);
+        $locator = sprintf('table.availability-form-table tbody td[data-day="%d"] input[type="checkbox"]', (int) date('w', $time) + 1);
+        $locator .= 'checked' === $state ? ':checked' : ':not(:checked)';
+        $elements = $page->findAll('css', $locator);
+        if (12 > \count($elements)) {
+            throw new ExpectationException(sprintf('Some checkboxes of column "%s" are not checked.', $day), $this->minkContext->getSession()->getDriver());
+        }
+    }
+}
