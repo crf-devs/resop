ccbe69cf5aa990b216f8f98f96b6cfe958eb1561
gcodeur <mail@gcodeur.eu>
2020-04-14 23:37:00 +0200

    Resolves #278: Displays skills in users list

---
+++ src/Twig/Extension/UserExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig\Extension;
+
+use App\Domain\SkillSetDomain;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+final class UserExtension extends AbstractExtension
+{
+    private SkillSetDomain $skillSetDomain;
+
+    public function __construct(SkillSetDomain $skillSetDomain)
+    {
+        $this->skillSetDomain = $skillSetDomain;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('skillBadge', [$this, 'formatBadge'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    public function formatBadge(string $skill): string
+    {
+        $importantSkills = $this->skillSetDomain->getImportantSkills();
+
+        return sprintf('<span class="badge badge-%s">%s</span>', \in_array($skill, $importantSkills, true) ? 'primary' : 'secondary', $skill);
+    }
+}
