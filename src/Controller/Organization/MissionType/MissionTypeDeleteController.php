25e0514dec3b082b9ebdeee8cf733f58ce767822
Olivier Mairet <oliviermairet@gmail.com>
2020-04-17 19:35:44 +0200

    fix(mission_type): add missing translations + few fixes in controller

--- src/Controller/Organization/MissionType/MissionTypeDeleteController.php
+++ src/Controller/Organization/MissionType/MissionTypeDeleteController.php
@@ -16,33 +16,30 @@ use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
- * @Route("/delete/{id}", name="app_organization_mission_type_delete", methods={"GET"})
+ * @Route("mission_type/delete/{id}", name="app_organization_mission_type_delete", methods={"GET"})
  * @Security("missionType.organization == user")
  * @IsGranted("ROLE_PARENT_ORGANIZATION")
  */
 class MissionTypeDeleteController extends AbstractController
 {
-    /**
-     * @var TranslatorInterface
-     */
     private TranslatorInterface $translator;
+    private EntityManagerInterface $entityManager;
 
-    public function __construct(TranslatorInterface $translator)
+    public function __construct(TranslatorInterface $translator, EntityManagerInterface $entityManager)
     {
         $this->translator = $translator;
+        $this->entityManager = $entityManager;
     }
 
-    public function __invoke(EntityManagerInterface $entityManager, MissionType $missionType): RedirectResponse
+    public function __invoke(MissionType $missionType): RedirectResponse
     {
-        $organization = $this->getUser();
-        if (!$organization instanceof Organization || false === $organization->isParent()) {
+        /** @var Organization $this->getUser() */
+        if (!$this->getUser() instanceof Organization || false === $this->getUser()->isParent()) {
             throw new AccessDeniedException();
         }
 
-        $entityManager->beginTransaction();
-        $entityManager->remove($missionType);
-        $entityManager->flush();
-        $entityManager->commit();
+        $this->entityManager->remove($missionType);
+        $this->entityManager->flush();
 
         $this->addFlash('success', $this->translator->trans('organization.mission_type.delete_success_message'));
 
