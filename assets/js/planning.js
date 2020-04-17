d59a58f90dcf2abde55130cbf59aabe19cd782c6
Nicolas PHILIPPE <nikophil@gmail.com>
2020-04-17 10:12:47 +0200

    Planning: Handle case when no asset or user available (#312)
    * Planning: Handle case when no asset or user available
    
    * Planning: fixed sort
    
    Since we've duplicated headers, 'sorted' and 'loadding' classes were buggy

--- assets/js/planning.js
+++ assets/js/planning.js
@@ -65,9 +65,10 @@ function toggleMoreInfos() {
 
 function dateSortPlanning($clickedTh, $planning) {
   let day = $clickedTh.data('day');
+  let $thWithSameDay = $('th[data-day="' + day + '"]');
 
-  $clickedTh.siblings('th[data-day]').removeClass('loading').removeClass('sorted');
-  $clickedTh.removeClass('sorted').addClass('loading');
+  $thWithSameDay.siblings('th[data-day]').removeClass('loading').removeClass('sorted');
+  $thWithSameDay.removeClass('sorted').addClass('loading');
 
   // We need to wait for the adding of loading class before doing this udge operation
   setTimeout(function () {
@@ -87,7 +88,7 @@ function dateSortPlanning($clickedTh, $planning) {
         })
         .appendTo($tbody);
 
-      $clickedTh.removeClass('loading').addClass('sorted');
+      $thWithSameDay.removeClass('loading').addClass('sorted');
     });
   });
 }
