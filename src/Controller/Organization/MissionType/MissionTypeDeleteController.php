ccec78c8e1975ceb57495c1f23eed89d8389512f
Olivier Mairet <oliviermairet@gmail.com>
2020-04-17 16:58:11 +0200

    refacto(mission_type): add translation for confirmation message

--- src/Controller/Organization/MissionType/MissionTypeDeleteController.php
+++ src/Controller/Organization/MissionType/MissionTypeDeleteController.php
@@ -7,16 +7,29 @@ namespace App\Controller\Organization\MissionType;
 use App\Entity\MissionType;
 use App\Entity\Organization;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
  * @Route("/delete/{id}", name="app_organization_mission_type_delete", methods={"GET"})
  */
 class MissionTypeDeleteController extends AbstractController
 {
+    /**
+     * @var TranslatorInterface
+     */
+    private TranslatorInterface $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
     public function __invoke(EntityManagerInterface $entityManager, MissionType $missionType): RedirectResponse
     {
         $organization = $this->getUser();
@@ -29,7 +42,7 @@ class MissionTypeDeleteController extends AbstractController
         $entityManager->flush();
         $entityManager->commit();
 
-        $this->addFlash('success', 'Le type de mission a été supprimé avec succès.');
+        $this->addFlash('success', $this->translator->trans('organization.mission_type.delete_success_message'));
 
         return $this->redirectToRoute('app_organization_mission_type_index');
     }
