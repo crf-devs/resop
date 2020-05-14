249606d18e430448107c9aec0c088a9bc1940bc2
Nicolas PHILIPPE <nikophil@gmail.com>
2020-05-14 18:26:26 +0200

    User dynamic properties: Migrate data

--- src/Migrations/Version20200512102054.php
+++ src/Migrations/Version20200512102054.php
@@ -22,7 +22,18 @@ final class Version20200512102054 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('ALTER TABLE users ADD properties JSON NOT NULL');
+        $this->addSql('ALTER TABLE users ADD properties JSON');
+        $this->addSql(
+            "UPDATE users set properties = json_build_object(
+                'organizationOccupation', organization_occupation,
+                'fullyEquipped', fully_equipped,
+                'drivingLicence', driving_licence,
+                'vulnerable', vulnerable,
+                'occupation', occupation
+            )"
+        );
+        $this->addSql('ALTER TABLE users alter column properties set not null;');
+
         $this->addSql('ALTER TABLE users DROP organization_occupation');
         $this->addSql('DROP INDEX user_fully_equipped_idx');
         $this->addSql('ALTER TABLE users DROP fully_equipped');
